e3f6cbdc901967e6c1924bee6258d245991b9502
Sean Handley <sean.handley@gmail.com>
2010-09-29 09:33:09 +0100

    ...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlhAchieve::Application.config.secret_token = '0bf0e33122b262712d240b118198d743f3f4895d0db478c634c99cda070eb175e50bc56be3f80583d5c613665e59157b79835dc5284c6db812be927978e33b43'
